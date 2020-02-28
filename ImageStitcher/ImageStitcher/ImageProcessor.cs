bcdbaa63115bf1c4a435e6d43156084c62d06963
WaterKH <waterkh@outlook.com>
2020-02-28 16:59:55 -0500

    Initial Commit

---
+++ ImageStitcher/ImageStitcher/ImageProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+
+namespace ImageStitcher
+{
+    public class ImageProcessor
+    {
+        public string Process(int number, string location, string saveNamePrev = "", int width = 256, int height = 256)
+        {
+            var files = Directory.GetFiles(location);
+
+            var saveName = string.IsNullOrEmpty(saveNamePrev) ? files.FirstOrDefault().Split('\\')[^1] : saveNamePrev;
+
+            using var stitchedImage = string.IsNullOrEmpty(saveNamePrev) ? new Image<Rgba32>(number * width, (files.Length / number) * height) : Image.Load(Path.Combine(Environment.CurrentDirectory, @"FinalImage\") + saveNamePrev);
+
+            var yOffset = 0;
+
+            for (int i = 0; i < files.Length; ++i)
+            { 
+                using var image = Image.Load(files[i]);
+
+                if (i % number == 0 && i != 0)
+                    yOffset++;
+
+                stitchedImage.Mutate(o => o.DrawImage(image, new Point((i % number) * width, yOffset * height), 1f));
+            }
+
+            foreach (var file in files)
+            {
+                File.Delete(file);
+            }
+
+            stitchedImage.Save(Path.Combine(Environment.CurrentDirectory, @"FinalImage\") + saveName);
+
+            return saveName;
+        }
+    }
+}
---
+++ ImageStitcher/ImageStitcher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ImageStitcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("How many images per row: ");
+                var input = Console.ReadLine();
+
+                int.TryParse(input, out var imagesPerRow);
+
+                Console.Write("How many layers: ");
+                input = Console.ReadLine();
+
+                int.TryParse(input, out var imageLayers);
+
+                var saveName = "";
+                if (imagesPerRow > 0)
+                {
+                    var imageProcessor = new ImageProcessor();
+
+                    for (int i = 1; i <= imageLayers; ++i)
+                    {
+                        var location = Environment.CurrentDirectory + "/ImagesToConvert/Layer" + i;
+                        saveName = imageProcessor.Process(imagesPerRow, location, saveName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                File.WriteAllText(Environment.CurrentDirectory + "/ErrorLog.txt", e.Message);
+                throw;
+            }
+        }
+    }
+}
